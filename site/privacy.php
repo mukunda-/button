19ff17627fe4ba32355ad7f66d45037aa17af76d
mukunda- <mukunda@mukunda.com>
2014-09-18 22:17:35 -0600

    more

--- site/privacy.php
+++ site/privacy.php
@@ -1,12 +1,18 @@
 <div class="topic nothing" id="topic">
+	<img src="logo256.png">
 	<p>privacy policy</p>
 	
-	<p>matbox is an anonymous accountless social media platform (AASMP)</p>
+	<p>matbox is an accountless anonymous social media platform (AASMP).</p>
 	
-	<p>accountless means we track your ip address to know who you are. this means that 
-	certain activities on the site, mainly votes, might be visible to anyone
+	<p>accountless means we need to track your ip address to know who you are. this means that 
+	certain activities on the site, like votes, might be visible to anyone
 	sharing your ip. we try to be as transparent as possible, but keeping this data
 	is required for us to prevent abuse.</p>
-	 
 	
+	<p>when you connect to matbox, an anonymous account will be created for your ip.
+	matbox promises to not share your ip with anyone.</p>
+	
+	<div class="panel">
+		<div class="button" onclick="matbox.Loader.RefreshContent()">okay.</div> 
+	</div>
 </div>
